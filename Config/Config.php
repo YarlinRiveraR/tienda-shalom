36f729bfb623d31e5a41d709efca651295479c0e
YarlinRiveraR <yarlinanyelisrr@ufps.edu.co>
2024-06-17 22:50:47 -0500

    add files

---
+++ Config/Config.php
@@ -0,0 +1,16 @@
+<?php
+const BASE_URL = "http://localhost/tienda-online/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "tienda-online";
+const CHARSET = "charset=utf8";
+const TITLE = "TIENDA ONLINE";
+const MONEDA = "USD";
+const CLIENT_ID = "AQHEaKqx7wGj892mdHzChyAoHtUBbttQB0cGRdSenDHd60EUpBAfb-5oZ8XTnc83wPPKticfsAAtwN2E";
+
+const USER_SMTP = "shalom.pijamas.notificaciones@gmail.com";
+const PASS_SMTP = "ramqcpwnfexdwtec";
+const PUERTO_SMTP = 465;
+const HOST_SMTP = "smtp.gmail.com";
+?>
